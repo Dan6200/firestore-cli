59f6dc1280efd37df32ab83f20c98471b5627b6d
Daniel Nyong <danielnyong66@gmail.com>
2025-11-01 11:14:54 +0100

    Set up jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,36 +1,7 @@
-import { pathsToModuleNameMapper } from "ts-jest";
-import { readFileSync } from "fs";
-
-const { compilerOptions } = JSON.parse(readFileSync("./tsconfig.json"));
-
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-
-// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
 const config = {
-  coverageProvider: "v8",
   testEnvironment: "node",
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  preset: "ts-jest",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.mts"], // Optional: for setup files
-  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
-  transform: {
-    "^.+\\.(ts|js|mts|mjs)$": [
-      "ts-jest",
-      { tsconfig: "tsconfig.json", useESM: true },
-    ], // Enable ESM for ts-jest
-  },
-  moduleFileExtensions: ["ts", "mts", "js", "mjs"], // Must include to make jest "see" what filetypes are tests
-  extensionsToTreatAsEsm: [".ts", ".mts"], // Tell Jest that these extensions should be treated as ESM
-  testMatch: [
-    "<rootDir>/tests/*.?(m)[tj]s?(x)", // Must match directory structure
-    "<rootDir>/tests/**/*.test.?(m)[tj]s?(x)", // Match test files with .test.ts, .test.js etc.
-    "<rootDir>/tests/**/test.?(m)[tj]s?(x)", // Match a single test file like test.ts, test.js
-  ],
-  resolver: "jest-ts-webcompat-resolver",
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: "<rootDir>/",
-  }),
+  testMatch: ["<rootDir>/built/tests/**/*.mjs"],
 };
 
 export default config;
